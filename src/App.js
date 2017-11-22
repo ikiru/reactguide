172f5f7749a19508b5cdbc4922d46fac80ca856f
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-22 11:22:51 -0600

    added bind across components

--- src/App.js
+++ src/App.js
@@ -10,15 +10,32 @@ class App extends Component {
       {name: 'Julie', age:27 }
     ]
   }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons:[
+        {name: newName, age:28 },
+        {name: 'Manu', age:30 },
+        {name: 'Julie', age:27 }
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age} >My Hobbies: Racing </Person>
-        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Judith!!!!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name}  
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Judith!!!!!!!')}>My Hobbies: Racing </Person>
+        <Person 
+          name={this.state.persons[2].name}  
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
