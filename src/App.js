8571aab99f9790f6dd4b4ae45912805990d89796
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-22 10:32:20 -0600

    added states and button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons:[
+      {name: 'Max', age:28 },
+      {name: 'Manu', age:30 },
+      {name: 'Julie', age:27 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name='Max' age='28'/>
-        <Person name='Manu' age='29'>My Hobbies: Racing </Person>
-        <Person name='Julie' age='26'/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age} >My Hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age} />
       </div>
     );
   }
