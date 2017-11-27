a0353a47666493c33ce0582aca5fdde24e40be0e
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-27 13:57:11 -0600

    added toggle on and off with true function

--- src/App.js
+++ src/App.js
@@ -48,6 +48,28 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+     
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name}  
+            age={this.state.persons[1].age} 
+            click={this.switchNameHandler.bind(this, 'Judith!!!!!!!')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
+          <Person 
+            name={this.state.persons[2].name}  
+            age={this.state.persons[2].age}/>
+          </div> 
+      );
+    }
+
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -55,22 +77,7 @@ class App extends Component {
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name</button>
-
-        { this.state.showPersons ? 
-          <div>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name}  
-          age={this.state.persons[1].age} 
-          click={this.switchNameHandler.bind(this, 'Judith!!!!!!!')}
-          changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
-        <Person 
-          name={this.state.persons[2].name}  
-          age={this.state.persons[2].age}/>
-          </div>  : null
-        }
+      {persons}
       </div>
     );
   }
