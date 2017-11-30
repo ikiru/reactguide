4dfa6155f5ec90a9d6ce05989f44d4754f01a9af
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-29 22:32:11 -0600

    fixed all errors

--- src/App.js
+++ src/App.js
@@ -12,18 +12,24 @@ class App extends Component {
     ]
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons:[
-        {name: 'Max', age:28 },
-        {name: event.target.value, age:30 },
-        {name: 'Julie', age:27 }
-      ]
-    })
-  }
+  nameChangedHandler = (event, id) => {
+    const personIndex =this.state.persons.findIndex(p =>{
+      return p.id == id;
+    });
+
+    const person ={
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons} );
+    }
 
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
     const persons =[...this.state.persons];
     persons.splice(personIndex,1);
     this.setState({persons: persons});
@@ -54,7 +60,8 @@ class App extends Component {
             click={() => this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
-            key={person.id}/>
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
           </div> 
       );
