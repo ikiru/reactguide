777b18760f66e99b487907a676c218ae74fb92b1
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-12-04 07:47:38 -0600

    added more log file to show whats happening in each section

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,33 +11,46 @@ class Person extends Component {
         console.log('====================================');
         console.log('[Person.js] Inside constructor', props);
         console.log('====================================');
-      }
+    }
     
     componentWillMount (){
         console.log('====================================');
         console.log('[Person.js] Inside componentWillMount()');
         console.log('====================================');
-      }
+    }
     
-      componentDidMount (){
+      componentDidMount () {
         console.log('====================================');
         console.log('[Person.js] Inside the componentDidMount()');
         console.log('====================================');
-      }
+    }
 
-    render (){
+    componentWillRecieveProps(nextProps) {
+        console.log('====================================');
+        console.log('[UPDATE Person.js] Inside componentWillRecieveProps', nextProps );
+        console.log('====================================');
+    }
+
+    render() {
         console.log('====================================');
         console.log('[Person.js] inside render ()');
         console.log('====================================');
+    
 
-        return (
+        return this.props.persons.map(( person, index ) => { 
+            return <Person
+                click={() => this.props.clicked( index )}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.props.changed( event, person)} />
+        });
             <div className={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type='text' onChange={this.props.changed} value={this.props.name}/>
             </div>
                     
-         )
     }
 } 
 
